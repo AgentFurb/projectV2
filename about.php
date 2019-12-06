143464a4679764a96d2d1519b089c31653185514
Unknown <ferdyh97@hotmail.nl>
2019-12-06 11:31:25 +0100

    Primary commit
    Required files added, folder structure complete, bootstrap files loaded

---
+++ about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'inc/header.php';?>
+    <title>Project V2 - Delta Space</title>
+</head>
+<body>
+
+    
+    
+<?php include 'inc/footer.php';?>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,11 @@
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="description" content="Project V2, a rocket build by Delta Space">
+    <meta name="keywords" content="HTML,CSS,JavaScript, rocket, delta space, space, rocket, project v2">
+    <meta name="author" content="Ferdy Hommeles">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
