afdf852812208063f9f64cf77d22a769e37020d0
Unknown <ferdyh97@hotmail.nl>
2019-12-09 10:03:44 +0100

    layout setup
    nav. svg, footer

--- index.php
+++ index.php
@@ -5,8 +5,16 @@
     <title>Project V2 - Delta Space</title>
 </head>
 <body>
-
-    
+<div class="container">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,160L80,138.7C160,117,320,75,480,96C640,117,800,203,960,213.3C1120,224,1280,160,1360,128L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+    <!-- Image and text -->
+    <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="/docs/4.4/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Bootstrap
+    </a>
+    </nav>
+</div>    
     
 <?php include 'inc/footer.php';?>
 </body>
