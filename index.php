163cd961cb11f46cda3afacf97959eaa311ecbe2
Unknown <ferdyh97@hotmail.nl>
2019-12-10 11:57:00 +0100

    Added png's and completed the layout

--- index.php
+++ index.php
@@ -9,12 +9,39 @@
     <svg id="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#0099ff" fill-opacity="1" d="M0,96L80,133.3C160,171,320,245,480,240C640,235,800,149,960,128C1120,107,1280,149,1360,170.7L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
     </svg>
-    <div>
-        
+<!-- <nav class="navbar navbar-light bg-transparent">
+  <a class="navbar-brand" href="#">
+    <img src="assets/img/v2-rocket.png" width="50" height="50" class="d-inline-block align-middle" alt="">
+    Project V2
+  </a>
+</nav> -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light bg-transparent ">
+    <a class="navbar-brand" href="#">
+        <img src="assets/img/ds-logo-light.png" width="100" height="100" class="d-inline-block align-middle" alt="">
+        Delta Space
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+        <a class="nav-link text-center" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link text-center" href="#">Over ons</a>
+        </li>
     </div>
+    <img src="assets/img/stars.png" class="stars" alt="">
+</nav>
+<img src="assets/img/v2-logo.png" class="v2-logo" alt="">
+<img src="assets/img/v2-rocket.png" class="rocket-img" alt="">
+<a class="launch-btn text-center" href="#">Launch</a>
 </section>
 <section class="sec2">
-
+    
 </section>    
     
 <?php include 'inc/footer.php';?>
